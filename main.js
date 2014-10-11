cd024e7ac1359b2b762a185eca49d9eb23b1ecee
TomMalbran <tom@Tom.local>
2014-10-11 19:57:03 -0300

    Update to Brackets 0.44

--- main.js
+++ main.js
@@ -21,7 +21,7 @@
  * 
  */
 
-/*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50 */
+/*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50, unparam: true */
 /*global define, $, window, brackets, Mustache */
 
 define(function (require, exports, module) {
@@ -31,68 +31,185 @@ define(function (require, exports, module) {
         ExtensionUtils     = brackets.getModule("utils/ExtensionUtils"),
         StringUtils        = brackets.getModule("utils/StringUtils"),
         DocumentManager    = brackets.getModule("document/DocumentManager"),
-        EditorManager      = brackets.getModule("editor/EditorManager"),
         LanguageManager    = brackets.getModule("language/LanguageManager"),
+        MainViewFactory    = brackets.getModule("view/MainViewFactory"),
         ProjectManager     = brackets.getModule("project/ProjectManager"),
         FileSystem         = brackets.getModule("filesystem/FileSystem"),
         FontHolderTemplate = require("text!htmlContent/font-holder.html"),
+        _                  = brackets.getModule("thirdparty/lodash"),
         
     /* List of Supported font Extensions */
-        FONT_EXTENSIONS = ["ttf", "otf", "woff"];
+        FONT_EXTENSIONS = ["ttf", "otf", "woff"],
+    
+    /** @type {Array.<FontView>} */
+        _viewers = {};
     
     
-    
-    /**
-     * Update file name if necessary
-     */
-    function onFileNameChange(e, oldName, newName) {
-        var $element    = $(".font-holder .font-path"),
-            oldRelPath  = ProjectManager.makeProjectRelativeIfPossible(oldName),
-            currentPath = $element.text();
-
-        if (currentPath === oldRelPath) {
-            var newRelName = ProjectManager.makeProjectRelativeIfPossible(newName);
-            $element.text(newRelName).attr("title", newRelName);
-        }
-    }
     
     /** 
      * Sign off listeners when editor manager closes the font viewer
      */
-    function onRemove() {
-        $(DocumentManager).off("fileNameChange", onFileNameChange);
-    }
+//    function onRemove() {
+//        $(DocumentManager).off("fileNameChange", onFileNameChange);
+//    }
     
     
-    /** 
-     * Perform decorations on the view that require loading the font in the browser
-     * @param {!string} fullPath Path to the image file
-     * @param {!jQueryObject} $editorHolder The DOM element to append the view to.
+    /**
+     * FontView objects are constructed when an font is opened 
+     * @constructor
+     * @param {!File} file  The image file object to render
+     * @param {!jQuery} container  The container to render the image view in
      */
-    function render(fullPath, $editorHolder) {
-        var file          = FileSystem.getFileForPath(fullPath),
-            relPath       = ProjectManager.makeProjectRelativeIfPossible(fullPath),
-            $customViewer = $(Mustache.render(FontHolderTemplate, {
-                fullPath : fullPath,
-                relPath  : relPath
-            }));
+    function FontView(file, $container) {
+        this.file    = file;
+        this.relPath = ProjectManager.makeProjectRelativeIfPossible(this.file.fullPath);
+        this.$el     = $(Mustache.render(FontHolderTemplate, {
+            fullPath : this.file.fullPath,
+            relPath  : this.relPath,
+            now      : new Date().valueOf()
+        }));
         
-        file.stat(function (err, stat) {
+        $container.append(this.$el);
+        
+        this.$fontFace = this.$el.find(".font-face");
+        this.$fontPath = this.$el.find(".font-path");
+        
+        // Update the file stats
+        var self = this;
+        this.file.stat(function (err, stat) {
             if (!err && stat._size) {
                 var dataString = StringUtils.prettyPrintBytes(stat._size, 2);
-                $(".font-holder .font-data").text(dataString).attr("title", dataString);
-                
-                $customViewer.show();
+                self.$el.find(".font-holder .font-data").text(dataString).attr("title", dataString);
             }
         });
-
-        // place DOM node to hold image
-        $editorHolder.append($customViewer);
         
         // make sure we always show the right file name
-        $(DocumentManager).on("fileNameChange", onFileNameChange);
+        $(DocumentManager).on("fileNameChange", _.bind(this._onFilenameChange, this));
         
-        return $customViewer;
+        _viewers[file.fullPath] = this;
+    }
+    
+    /**
+     * DocumentManger.fileNameChange handler - when a font is renamed, we must 
+     * update the view
+     * 
+     * @param {jQuery.Event} e  event
+     * @param {!string} oldPath  the name of the file that's changing changing 
+     * @param {!string} newPath  the name of the file that's changing changing 
+     * @private
+     */
+    FontView.prototype._onFilenameChange = function (e, oldPath, newPath) {
+        // File objects are already updated when the event is triggered
+        // so we just need to see if the file has the same path as our image
+        if (this.file.fullPath === newPath) {
+            this.relPath = ProjectManager.makeProjectRelativeIfPossible(newPath);
+            this.$fontPath.text(this.relPath).attr("title", this.relPath);
+        }
+    };
+    
+    /**
+     * View Interface functions
+     */
+
+    /* 
+     * Retrieves the file object for this view
+     * return {!File} the file object for this view
+     */
+    FontView.prototype.getFile = function () {
+        return this.file;
+    };
+    
+    /* 
+     * Updates the layout of the view
+     */
+    FontView.prototype.updateLayout = function () {
+        var $container = this.$el.parent(),
+            pos        = $container.position(),
+            iWidth     = $container.innerWidth(),
+            iHeight    = $container.innerHeight(),
+            oWidth     = $container.outerWidth(),
+            oHeight    = $container.outerHeight();
+            
+        // $view is "position:absolute" so 
+        //  we have to update the height, width and position
+        this.$el.css({
+            top    : pos.top  + ((oHeight - iHeight) / 2),
+            left   : pos.left + ((oWidth  - iWidth)  / 2),
+            width  : iWidth,
+            height : iHeight
+        });
+    };
+    
+    /* 
+     * Destroys the view
+     */
+    FontView.prototype.destroy = function () {
+        delete _viewers[this.file.fullPath];
+        $(DocumentManager).off("fileNameChange", _.bind(this._onFilenameChange, this));
+        this.$el.remove();
+    };
+    
+    /* 
+     * Refreshes the image preview with what's on disk
+     */
+    FontView.prototype.refresh = function () {
+        var noCacheUrl = this.$fontFace.data("src"),
+            now        = new Date().valueOf(),
+            index      = noCacheUrl.indexOf("?");
+
+        // Strip the old param off 
+        if (index > 0) {
+            noCacheUrl = noCacheUrl.slice(0, index);
+        }
+        
+        // Add a new param which will force chrome to re-read the image from disk 
+        noCacheUrl = noCacheUrl + "?ver=" + now;
+
+        // Update the DOM node with the src URL 
+        this.$fontFace.html("@font-face {font-family:'FontDisplay';src: url('" + noCacheUrl + "');}");
+    };
+    
+    
+    
+    /**
+     * Creates an image view object and adds it to the specified pane
+     * @param {!File} file - the file to create an image of
+     * @param {!Pane} pane - the pane in which to host the view
+     * @return {jQuery.Promise} 
+     */
+    function _createFontView(file, pane) {
+        var view = pane.getViewForPath(file.fullPath);
+        
+        if (view) {
+            pane.showView(view);
+        } else {
+            view = new FontView(file, pane.$content);
+            pane.addView(view, true);
+        }
+        return new $.Deferred().resolve().promise();
+    }
+    
+    /**
+     * Handles file system change events so we can refresh image viewers for the files that changed on disk due to external editors
+     * @param {jQuery.event} event  event object
+     * @param {?File} file  file object that changed
+     * @param {Array.<FileSystemEntry>=} added  If entry is a Directory, contains zero or more added children
+     * @param {Array.<FileSystemEntry>=} removed  If entry is a Directory, contains zero or more removed children
+     */
+    function _handleFileSystemChange(event, entry, added, removed) {
+        // this may have been called because files were added 
+        //  or removed to the file system.  We don't care about those
+        if (!entry || entry.isDirectory) {
+            return;
+        }
+        
+        // Look for a viewer for the changed file
+        var viewer = _viewers[entry.fullPath];
+
+        // viewer found, call its refresh method
+        if (viewer) {
+            viewer.refresh();
+        }
     }
     
     
@@ -106,10 +223,24 @@ define(function (require, exports, module) {
         isBinary       : true
     });
     
-    /** Register the Custom Viewer */
-    EditorManager.registerCustomViewer("font", {
-        render   : render,
-        onRemove : onRemove
+    
+    /*
+     * Install an event listener to receive all file system change events
+     * so we can refresh the view when changes are made to the image in an external editor
+     */
+    FileSystem.on("change", _handleFileSystemChange);
+    
+    /* 
+     * Initialization, register our view factory
+     */
+    MainViewFactory.registerViewFactory({
+        canOpenFile: function (fullPath) {
+            var lang = LanguageManager.getLanguageForPath(fullPath);
+            return (lang.getId() === "font");
+        },
+        openFile: function (file, pane) {
+            return _createFontView(file, pane);
+        }
     });
     
     AppInit.appReady(function () {
